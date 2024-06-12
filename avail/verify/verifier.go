5b845dae2727acdf71f642e735101febd8a9a04d
prampey <prudhvi.rampey@gmail.com>
2024-06-12 04:20:15 +0000

    refactor(avail): contract binding packages

--- avail/verify/verifier.go
+++ avail/verify/verifier.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/stackrlabs/go-daash/avail"
-	"github.com/stackrlabs/go-daash/avail/verify/bindings/vectorverifier"
+	vectorverifier "github.com/stackrlabs/go-daash/avail/verify/vector/verifier"
 )
 
 const (
